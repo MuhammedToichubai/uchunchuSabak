a3c523d10a3f11f50743497a015025d175a8d6b8
Muhammed <muhammedtoichubai@gmail.com>
2022-02-07 00:12:41 -0600

    commit two

---
+++ src/com/company/Main.java
@@ -0,0 +1,130 @@
+package com.company;
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//   int a = 10;
+//   a++;
+//   a++;
+//   a--;
+//        System.out.println(a);
+//        //joop 11
+//        Random muhammed = new Random();
+//        int kokusSan = muhammed.nextInt();
+//        System.out.println(kokusSan);
+
+//        Scanner kyrgyz =new Scanner(System.in);
+//        short kg = kyrgyz.nextShort();
+//        short a , b, c;
+//        a =5;
+//        b =3;
+//        c =10;
+//        System.out.println(kg*a );
+//        short kg1 = kyrgyz.nextShort();
+//        System.out.println(kg*b );
+//        short kg2 = kyrgyz.nextShort();
+//        System.out.println( kg2-c);
+//
+//        Scanner men =new Scanner(System.in);
+//        int kg = men.nextShort();
+//        int kg1 = men.nextShort();
+//        int osh = Math.min(kg, kg1);
+//        System.out.println("Siz bergen 2 sandyn kichigi-" +osh);
+
+//        Scanner sen = new Scanner(System.in);
+//        short a = sen.nextShort();
+//        short d = sen.nextShort();
+//        double s = Math.pow( a ,d );
+//        System.out.println(s);
+
+          Scanner bolchok = new Scanner(System.in);
+          double z = bolchok.nextDouble();
+          double v = bolchok.nextDouble();
+          double sum = z+v;
+          double s =Math.ceil(sum);
+          System.out.println(s);
+          double n =Math.floor(sum);
+          System.out.println(n);
+//
+//
+//        System.out.println((m1+m2+m3+m4+m5+m6+m7+m8+m9+m10)/10);
+
+        
+//        Random random = new Random();
+//        float p = random.nextFloat();
+//        System.out.print(p +"|");
+//        float o = random.nextFloat();
+//        System.out.print(o +"|");
+//        float i = random.nextFloat();
+//        System.out.println(i +"");
+//        float u = random.nextFloat();
+//        System.out.println("--------------------------------");
+//        System.out.print(u +"|");
+//        float y = random.nextFloat();
+//        System.out.print(y +"|");
+//        float t = random.nextFloat();
+//        System.out.println(t +"");
+//        System.out.println("--------------------------------");
+//        float r = random.nextFloat();
+//        System.out.print(r +"|");
+//        float e = random.nextFloat();
+//        System.out.print(e +"|");
+//        float w = random.nextFloat();
+//        System.out.println(w +"|");
+//        System.out.print("--------------------------------");
+
+//        Scanner dialog = new Scanner(System.in);
+//        String zz = dialog.next();
+//        String aa = dialog.next();
+//        int jash = dialog.nextInt();
+//        String bb = dialog.next();
+//        System.out.println("-Salam " +zz);
+//        System.out.println("-Menin atym " +aa +"!" +"Men " +jash +"jashtamyn,senchi?");
+//        System.out.println("-Men " +bb );
+
+
+//        Random random = new Random();
+//        int summaarif = 0;
+//        for (int i = 0; i <= 3; i++) {
+//            int san = random.nextInt(50);
+//            summaarif += san;
+//            if (summaarif != san) {
+//                System.out.print(san + " + ");
+//            }
+//        }
+//        System.out.print(" = "+summaarif+ "\n" +summaarif+ " / 3 = "+summaarif/3d);
+
+//    }
+        Scanner konsol = new Scanner(System.in);
+
+//        int a = konsol.nextInt();
+//        int b = konsol.nextInt();
+//
+//        if (a % b == 0) {
+//            System.out.println("bolunot");
+//        }else {
+//            System.out.println("bolunboit");
+//        }
+//        int a1 = konsol.nextInt();
+//        int a2 = konsol.nextInt();
+//        int a3 = konsol.nextInt();
+//        int muh1 = a1;
+//        int muh2 = a2;
+//        int muh3 = a3;
+//
+//        if (a1 == muh1 && a2 ==muh2   ){
+//            System.out.println(3);
+//        } else if (a2 == muh1 && a2 ==muh3 && a2 < muh3) {
+//            System.out.println(2);
+//        }  else  if (a3 == muh2 && a3 <=muh3 && a3 >=muh3) {
+//            System.out.println(1);
+//        }else{
+//
+//        }
+    }
+}
